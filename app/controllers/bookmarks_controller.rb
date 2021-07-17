9b0574fefa8ada8d20fa15126f77d48780ae0264
Jeniffer Callou <jenifferprc@gmail.com>
2021-07-17 15:39:18 +0200

    Added Controllers

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list  # very important to remember!!!!
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,41 +1,42 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_list, only: [ :show, :destroy ]
 
   def index
     @lists = List.all
   end
 
   def show
+    @bookmark = Bookmark.new
   end
 
   def new
     @list = List.new
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def create
     @list = List.new(list_params)
 
     if @list.save
-      redirect_to @list, notice: "List was successfully created."
+      redirect_to @list, notice: "List was created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def update
-    if @list.update(list_params)
-      redirect_to @list, notice: "List was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @list.update(list_params)
+  #     redirect_to @list, notice: "List was updated."
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
     @list.destroy
-    redirect_to lists_url, notice: "List was successfully destroyed."
+    redirect_to lists_url, notice: "List was destroyed."
   end
 
   private
